5757cb731dc0a89c122f19d7e899c1ae7d24af57
ChiaLoChen <94023698+ChiaLoChen@users.noreply.github.com>
2024-10-23 22:13:28 -0400

    level 2
    next level to test out singleton

--- Assets/Script/Factory/Labirynth/Wall.cs
+++ Assets/Script/Factory/Labirynth/Wall.cs
@@ -6,6 +6,6 @@ public class Wall : LabrynthCreation
 {
     public override void Create(Vector3 position, GameObject prefab)
     {
-        GameObject wall = GameObject.Instantiate(prefab, position, Quaternion.identity);
+        GameObject createPrefab = GameObject.Instantiate(prefab, position, Quaternion.identity);
     }
 }
